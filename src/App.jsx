10825e52a5823a22706873e1f72217d0f013377d
Zeyad Ayman <zeyad4WOnly@gmail.com>
2025-09-16 18:04:14 +0200

    Project is initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Canvas } from "@react-three/fiber"
+import { Camera } from "three"
+
+
+function App() {
+
+  return (
+    <>
+      <Canvas>
+      
+      </Canvas>
+    </>
+  )
+}
+
+export default App
