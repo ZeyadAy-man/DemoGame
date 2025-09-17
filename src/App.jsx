8161d79b0050a45f090b3c01dfa9a408b7fb4638
Zeyad Ayman <zeyad4WOnly@gmail.com>
2025-09-17 14:16:26 +0200

    Uploaded some models

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Canvas } from "@react-three/fiber"
 import { Camera } from "three"
+import { OrbitControls, useGLTF } from "@react-three/drei"
 
 
 function App() {
-
+  const { scene } = new useGLTF('');
   return (
     <>
-      <Canvas>
-      
+      <Canvas camera={{ position: [0, 0, 3], fov: 75 }} style={{height: "100vh", width: "100vw"}}>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+        <OrbitControls/>
       </Canvas>
     </>
   )
